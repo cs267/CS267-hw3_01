81a2c0ec33b8b1d41599eb1c94763016ca6da74e
Kolen Cheung <christian.kolen@gmail.com>
2017-03-13 00:18:04 -0700

    cleanup C style
    find . \( -iname "*.c" -o -iname "*.cpp" -o -iname "*.h" -o -iname
    "*.cu" -o -iname "*.upc" \) -exec clang-format -i -style=WebKit {} \+

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,30 +1,31 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-	string line;
-	vector<string> lines;
-	ifstream infile(argv[1]);
-	ofstream outfile(argv[2]);
-	vector<string>::size_type i;
+int main(int argc, char* argv[])
+{
+    string line;
+    vector<string> lines;
+    ifstream infile(argv[1]);
+    ofstream outfile(argv[2]);
+    vector<string>::size_type i;
 
-	lines.clear();
-	if(infile.is_open()){
-		while(getline(infile, line)){
-			lines.push_back(line);
-		}
-	}
+    lines.clear();
+    if (infile.is_open()) {
+        while (getline(infile, line)) {
+            lines.push_back(line);
+        }
+    }
 
-	sort(lines.begin(), lines.end());
-	for(i=0;i!=lines.size();i++)
-		outfile << lines[i] << endl;
+    sort(lines.begin(), lines.end());
+    for (i = 0; i != lines.size(); i++)
+        outfile << lines[i] << endl;
 
-	infile.close();
-	outfile.close();
-	return 0;
+    infile.close();
+    outfile.close();
+    return 0;
 }
